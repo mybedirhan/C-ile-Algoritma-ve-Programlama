520633b9a29ec9d7df3ad50cdc4f0d237a5b8538
mybedirhan <101818580+mybedirhan@users.noreply.github.com>
2023-01-08 16:02:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*   
+           Console.WriteLine("Merhaba Dünya");
+           Console.Read();
+        */
+        /*
+        Console.WriteLine("Sikici Bela 69");
+        Console.Read();
+        */
+        /*
+        string sehir, ilce, semt;
+        sehir = "İstanbul";
+        ilce = "Bahcelievler";
+        semt = "Yenibosna";
+        Console.Write(sehir + "/" + ilce + "/" + semt);
+        Console.Read();
+        */
+
+        /*
+        int sayi1, sayi2, toplam;
+
+        sayi1 = 31;
+        sayi2 = 69;
+        toplam = sayi1 + sayi2;
+
+        Console.Write(toplam);
+
+        Console.Read();
+        */
+
+        /*
+        string ad, soyad, okul, bolum, ders;
+        int s1, s2, s3, ort;
+
+        ad = "Bedirhan";
+        soyad = "KORKMAZ";
+        okul = "Düzce Üniversitesi";
+        bolum = "Bilgisayar Mühendisliği";
+        ders = "Nesneye Dayalı Programlama(OOP)";
+
+        s1 = 31;
+        s2 = 69;
+        s3 = 62;
+        ort = (s1 + s2 + s3) / 3;
+
+        Console.WriteLine("***** - Öğrenci Bilgi Sistemi - *****");
+        Console.WriteLine();
+        Console.WriteLine("***** - Kişisel Bilgiler - *****");
+        Console.WriteLine();
+        Console.WriteLine("Öğrencinin Adı ve Soyadı:" + ad + " " + soyad);
+        Console.WriteLine("Öğrencinin Okulu:" + okul);
+        Console.WriteLine("Öğrencinin Bölümü:" + bolum);
+        Console.WriteLine("Öğrencinin Dersi:" + ders);
+        Console.WriteLine();
+        Console.WriteLine("***** - Not Bilgisi - *****");
+        Console.WriteLine("Sınav 1:" + s1);
+        Console.WriteLine("Sınav 2:" + s2);
+        Console.WriteLine("Sınav 3:" + s3);
+        Console.WriteLine("Ortalaması:" + ort);
+        Console.WriteLine();
+        Console.WriteLine("***** - Öğrenci Bilgi Sistemi - *****");
+
+        Console.Read();
+        */
+
+
+        Console.Read();
+    }
+}
